77f256ed4cf967f249749f51227945a17d894456
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2019-06-27 20:17:00 -0400

    Errores y Entidades

--- Segundo-Parcial-EnmanuelPaulino/Segundo-Parcial-EnmanuelPaulino/Entidades/Asignaturas.cs
+++ Segundo-Parcial-EnmanuelPaulino/Segundo-Parcial-EnmanuelPaulino/Entidades/Asignaturas.cs
@@ -11,12 +11,12 @@ namespace Segundo_Parcial_EnmanuelPaulino.Entidades
     {
         [Key]
 
-        public int AsignaturaId { get; set; }
+        public int AsignaturasId { get; set; }
         public string Descripcion { get; set; }
         public double Creditos { get; set; }
         public Asignaturas()
         {
-            AsignaturaId = 0;
+            AsignaturasId = 0;
             Descripcion = string.Empty;
             Creditos = 0;
         }
