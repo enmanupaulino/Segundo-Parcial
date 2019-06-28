82c19b2be3a4a4d29cfa30ac96be70bc95b07106
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2019-06-27 20:51:17 -0400

    Contexto

--- Segundo-Parcial-EnmanuelPaulino/Segundo-Parcial-EnmanuelPaulino/DAL/Contexto.cs
+++ Segundo-Parcial-EnmanuelPaulino/Segundo-Parcial-EnmanuelPaulino/DAL/Contexto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Segundo_Parcial_EnmanuelPaulino.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,6 +10,13 @@ namespace Segundo_Parcial_EnmanuelPaulino.DAL
 {
     public class Contexto : DbContext
     {
-        public DbSet<>
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+        public DbSet<Asignaturas> Asignaturas { get; set; }
+        public DbSet<Inscripcion>Inscripcions { get; set; }
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
     }
 }
