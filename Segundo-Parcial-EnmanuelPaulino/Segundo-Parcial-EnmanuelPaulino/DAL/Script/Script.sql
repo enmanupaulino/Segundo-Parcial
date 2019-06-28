eaaf604aa91f82a17c6935dd630b63118a18bcf4
Emmanuel <Emmanuel@DESKTOP-B6P5QMH>
2019-06-27 20:12:27 -0400

    Avanzando

---
+++ Segundo-Parcial-EnmanuelPaulino/Segundo-Parcial-EnmanuelPaulino/DAL/Script/Script.sql
@@ -0,0 +1,25 @@
+create database InscripcionDb
+
+use InscripcionDb
+
+create table Asignaturas
+(
+AsignaturasId int primary key identity,
+Descripcion varchar (max),
+Creditos int
+
+)
+
+Create table Estudiantes (
+EstudianteId int primary key identity,
+FecheIngreso datetime,
+Nombres varchar (max),
+Balance int 
+
+)
+
+create table Inscripcion(
+InscripcionId int primary key identity,
+Fecha datetime,
+Monto int
+)
